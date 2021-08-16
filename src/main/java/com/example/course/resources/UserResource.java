f08582906b442708fcd4744404f57a92efbbfe3a
nicolastoledo <nicky.96@hotmail.com>
2021-08-16 02:11:46 -0300

    Endpoint para retornar os posts de um usuário e obtendo um post por id

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -65,4 +65,5 @@ public class UserResource {
 		User obj = service.findById(id);		
 		return ResponseEntity.ok().body(obj.getPosts());
 	}
+	
 }
---
+++ src/main/java/com/example/course/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.course.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.domain.Post;
+import com.example.course.repository.PostRepository;
+import com.example.course.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
